822b443cb064575a2d3bd07cd2f2de4f47c681af
xiaochenggit <707996290@qq.com>
2017-02-20 15:24:51 +0800

    rt
    rt

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+var myApp = angular.module('phonecat', ['ngRoute']);
+  myApp.config(['$routeProvider', function($routeProvider){
+                $routeProvider
+                .when('/phones',{templateUrl: 'partials/phone-list.html',controller: PhoneListCtrl})
+                // .when('/phones/:phoneId',{templateUrl: '../partials/phone-detail.html', controller: PhoneDetailCtrl})
+                .otherwise({redirectTo:'/phones'});
+       }]);
---
+++ app/js/controllers.js
@@ -0,0 +1,6 @@
+myApp.controller('PhoneListCtrl'['$scope', '$http',function($scope, $http){
+  $http.get('phones/phones.json').success(function(data) {
+   	$scope.phones = data;
+  });
+  $scope.orderProp = 'age';
+}]);
